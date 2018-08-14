ebddbe7d49785a5f6b3c9f71324b64a17107caa6
tuzhao <tuzhaocn@gmail.com>
2018-08-14 19:00:46 +0800

    增加一种重复使用tag的方式

--- FlycoTabLayout_Lib/src/main/java/com/flyco/tablayout/utils/FragmentChangeManager.java
+++ FlycoTabLayout_Lib/src/main/java/com/flyco/tablayout/utils/FragmentChangeManager.java
@@ -9,9 +9,17 @@ import java.util.ArrayList;
 public class FragmentChangeManager {
     private FragmentManager mFragmentManager;
     private int mContainerViewId;
-    /** Fragment切换数组 */
+    /**
+     * Fragment切换数组
+     */
     private ArrayList<Fragment> mFragments;
-    /** 当前选中的Tab */
+    /**
+     * 与fragment list对应的fragment tag list
+     */
+    private ArrayList<String> mTags;
+    /**
+     * 当前选中的Tab
+     */
     private int mCurrentTab;
 
     public FragmentChangeManager(FragmentManager fm, int containerViewId, ArrayList<Fragment> fragments) {
@@ -21,16 +29,38 @@ public class FragmentChangeManager {
         initFragments();
     }
 
-    /** 初始化fragments */
+    public FragmentChangeManager(FragmentManager fm, int containerViewId, ArrayList<Fragment> fragments, ArrayList<String> tags) {
+        this.mFragmentManager = fm;
+        this.mContainerViewId = containerViewId;
+        this.mFragments = fragments;
+        this.mTags = tags;
+        initFragments();
+    }
+
+    /**
+     * 初始化fragments
+     */
     private void initFragments() {
-        for (Fragment fragment : mFragments) {
-            mFragmentManager.beginTransaction().add(mContainerViewId, fragment).hide(fragment).commit();
+        if (null != mTags && mTags.size() == mFragments.size()) {
+            for (int index = 0; index < mFragments.size(); index++) {
+                Fragment fragment = mFragments.get(index);
+                if (!fragment.isAdded()) {
+                    String tag = mTags.get(index);
+                    mFragmentManager.beginTransaction().add(mContainerViewId, fragment, tag).hide(fragment).commit();
+                }
+            }
+        } else {
+            for (Fragment fragment : mFragments) {
+                mFragmentManager.beginTransaction().add(mContainerViewId, fragment).hide(fragment).commit();
+            }
         }
 
         setFragments(0);
     }
 
-    /** 界面切换控制 */
+    /**
+     * 界面切换控制
+     */
     public void setFragments(int index) {
         for (int i = 0; i < mFragments.size(); i++) {
             FragmentTransaction ft = mFragmentManager.beginTransaction();
